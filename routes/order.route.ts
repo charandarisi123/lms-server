0c480979f0d86d6c98f39d9ee642f524da244bfd
charandarisi123 <saicharandarisi@example.com>
2024-04-27 04:29:23 +0530

    first comment

---
+++ routes/order.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { authorizeRoles, isAutheticated} from "../middleware/auth";
+import { createOrder, getAllOrders} from "../controllers/order.controller";
+import { updateAccessToken } from "../controllers/user.controller";
+const orderRouter = express.Router();
+
+orderRouter.post("/create-order", updateAccessToken,isAutheticated, createOrder);
+
+orderRouter.get("/get-orders", updateAccessToken,isAutheticated, authorizeRoles("admin"), getAllOrders);
+
+export default orderRouter;
